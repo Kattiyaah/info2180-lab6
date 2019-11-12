c316c15a7b4dbb84d4e73f7f0f81a6345058d0ad
Kattiyaah <tonianhyman@yahoo.com>
2019-11-11 21:02:23 -0500

    Edited

--- apps.js
+++ apps.js
@@ -1,11 +1,15 @@
 "use strict";
 function my_fun(search) {
-	var element = document.getElementById("button").addEventListener("click", );
+	var element = document.getElementById("button").addEventListener("click");
 	var httpRequest = new XMLHttpRequest();
 	var url = "http://localhost:8080/superheroes.php";
 	httpRequest.onreadystatechange = dosomething(){
 		if (httpRequest.readyState === 4 && httpRequest.status === 200){
-			parseXMLData(this, search, element);
+			var response = httpRequest.responseText;
+			alert(response);
+		}
+		else{
+			alert('Problem Alert!');
 		}
 	}
 	httpRequest.open("GET",url,true);
